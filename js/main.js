0f2ac7f92194f89231f7ccccb2ae414d827040cf
EmaBrites <71233604+EmaBrites@users.noreply.github.com>
2021-05-28 14:49:37 -0300

    Integracion
    combinación de las tareas hechas hasta el 27/5

--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
-let captcha ="ApE241";
+"use strict";
+let TextoCaptcha= document.querySelector("#captcha");
+let IngresoCaptcha= document.querySelector("#ingreso-captcha");
+let BotonEnviar= document.querySelector("#boton-enviar");
+let respuesta= document.querySelector("#respuesta-captcha");
+let BotonRefresh= document.querySelector("#boton-refrescar");
 
-let captchaInDom= document.querySelector("#captcha");
-captchaInDom.innerHTML=captcha;
+let ALfaNumerico=[ 'A', 'B','C','D','E','F','G','a','b','c','d','e','f','g','0','1','2','3','4','5','6','7','8','9'];
+let ArregloVacio= [];
 
+for (let i=1; i<=5; i++){
+    ArregloVacio.push(ALfaNumerico[Math.floor(Math.random()* ALfaNumerico.length)]);
+}
 
-let submit = document.querySelector("#btn-enviar");
-let resultado=document.querySelector("#resultadocaptcha");
-
-
-submit.addEventListener("click",function (e){
-
-    let respuesta = document.querySelector("#input-respuesta");
-    if (respuesta.value == captcha){
-        
-        resultado.innerHTML="el captcha es válido";
-    }else
-    {
-        resultado.innerHTML="el captcha es inválido";
+TextoCaptcha.innerHTML= ArregloVacio.join('');
+TextoCaptcha.addEventListener('keyup', function(e){
+    
+    if(TextoCaptcha.textoAlfaNum==10 ){
+    
+        if (IngresoCaptcha.value==TextoCaptcha.innerHTML){
+        respuesta.classList.add("Captcha-correcto");
+        respuesta.innerHTML== "El captcha ingresado es correcto!";    
+        }else{
+        respuesta.classList.add("Captcha-incorrecto");
+        respuesta.innerHTML=="El captcha ingresado es incorrecto! Por favor intente nuevamente";
+        }
     }
-})
+});
+    
+//function Validar(IngresoCaptcha){
+//IngresoCaptcha.preventDefault();//Nose dónde ubicar esto para que haga el stop antes de mostrar los mensajes de captcha correcto o incorrecto
+//} 
+BotonEnviar.addEventListener('click', function(){
+    if (IngresoCaptcha.value==TextoCaptcha.innerHTML){
+     respuesta.classList.add("Captcha-correcto");
+     respuesta.innerHTML="Correcto!";
+    }else{
+      respuesta.classList.add("Captcha-incorrecto");
+      respuesta.innerHTML="Captcha incorrecto, por favor intente nuevamente";  
+    }
+});
+
+BotonRefresh.addEventListener('click', function(){
+    IngresoCaptcha.value="";
+    let refreshArreglo=[];
+    for (let j=1; j<=5; j++){
+        refreshArreglo.push(ALfaNumerico[Math.floor(Math.random()* ALfaNumerico.length)]);
+    } 
+    TextoCaptcha.innerHTML= refreshArreglo.join('');
+    respuesta=="";
+});
 
 
 
@@ -26,6 +56,13 @@ submit.addEventListener("click",function (e){
 
 
 
- 
+
+
+
+
+
+
+
+
 
 
