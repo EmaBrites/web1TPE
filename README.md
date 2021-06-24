c5f36e54d63aae497710d28de61c492602ab0db2
EmaBrites <71233604+EmaBrites@users.noreply.github.com>
2021-06-24 03:12:30 -0300

    agregar y editar

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ lista de tareas andre:
 	
 lista de tareas ema:
 	cambiar de columna a fila posicion de tabla dinamica en vista escritorio
-	agragar
-	editar
 
 https://60c2aab9917002001739d577.mockapi.io/bat/voluntarios
+<script src="https://kit.fontawesome.com/51f3b5475e.js" crossorigin="anonymous"></script>
+<i class="fas fa-check-circle"></i>
--- js/tabla.js
+++ js/tabla.js
@@ -1,120 +1,132 @@
 "use strict";
 document.addEventListener("DOMContentLoaded",iniciarTabla);
-async function iniciarTabla(){
-    // let voluntarios=[
-    // {
-    //     id:"1",
-    //     nombre:"Antonela",
-    //     apellido:"Valdez",
-    //     telefono:"2494609754",
-    //     email:"avaldez@gmail.com",
-    //     area:"administracion",
-    //     turno:"mañana",
-    // },
-    // {
-    //     id:"2",
-    //     nombre:"Carlos",
-    //     apellido:"Cardona",
-    //     telefono:"2494628754",
-    //     email:"ccardona@gmail.com",
-    //     area:"deposito",
-    //     turno:"mañana",
-    // },
-    // {
-    //     id:"3",
-    //     nombre:"Daniel",
-    //     apellido:"Fuentes",
-    //     telefono:"2494626018",
-    //     email:"dfuentes@gmail.com",
-    //     area:"deposito",
-    //     turno:"tarde",
-    // },
-    // {
-    //     id:"4",
-    //     nombre:"Florencia",
-    //     apellido:"Gomez",
-    //     telefono:"2494641444",
-    //     email:"fgomez@gmail.com",
-    //     area:"administracion",
-    //     turno:"tarde",
-    // }];
-    
+function iniciarTabla(){
+    const url="https://60c2aab9917002001739d577.mockapi.io/bat/voluntarios";
     mostrarTabla();
     document.querySelector("#btn-agregar").addEventListener("click",agrega1);
     document.querySelector("#btn-agregar3").addEventListener("click",agrega3);
     document.querySelector("#btn-borrar").addEventListener("click",borrar);
-    
-    
+    async function agrega1(e){
+        e.preventDefault();
+        let nombre=document.querySelector("#nombre").value;
+        let apellido=document.querySelector("#apellido").value;
+        let telefono=document.querySelector("#telefono").value;
+        let email=document.querySelector("#email").value;
+        let area=document.querySelector("#area").value;
+        let turno=document.querySelector("#turno").value;
 
-
-function agrega1(e){
-    e.preventDefault();
-    let nombre=document.querySelector("#nombre").value;
-    let apellido=document.querySelector("#apellido").value;
-    let telefono=document.querySelector("#telefono").value;
-    let email=document.querySelector("#email").value;
-    let area=document.querySelector("#area").value;
-    let turno=document.querySelector("#turno").value;
-
-    let voluntarioNuevo={
-        nombre: nombre,
-        apellido: apellido,
-        telefono: telefono,
-        email: email,
-        area: area,
-        turno: turno,
-    }
-    voluntarios.push(voluntarioNuevo);
-    mostrarTabla();
-}
-function agrega3(e){
-    e.preventDefault();
-    for(let i=0; i<3; i++){
-        agrega1(e);
-    }
-    mostrarTabla();
-}
-
-async function mostrarTabla(){
-    try{
-        let respuesta = await fetch("https://60c2aab9917002001739d577.mockapi.io/bat/voluntarios");
-        let voluntarios= await respuesta.json();
-        console.log(voluntarios);
-    
-        let tabla=document.querySelector("#t-voluntarios");
-        tabla.innerHTML=`
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Telefono</th>
-                    <th>email</th>
-                    <th>Area</th>
-                    <th>Turno</th>
-                </tr>
-            </thead>
-            `;
-        let cuerpo=document.createElement("tbody");
-        for (const item of voluntarios) {
-            let fila=document.createElement("tr");
-            fila.innerHTML = `
-                    <td>${item.nombre}</td>
-                    <td>${item.apellido}</td> 
-                    <td>${item.telefono}</td> 
-                    <td>${item.email}</td> 
-                    <td>${item.area}</td> 
-                    <td>${item.turno}</td>
-                `;
-            cuerpo.appendChild(fila);
+        let voluntarioNuevo={
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            email: email,
+            area: area,
+            turno: turno,
         }
-        tabla.appendChild(cuerpo);
-    }catch(error){
-    console.log(error);
-}
-}
-function borrar(e){
-    e.preventDefault();
-    voluntarios=[];   
-    mostrarTabla();
-}
+        try {
+            let res = await fetch(url, {
+                "method": "POST",
+                "headers": { "Content-type": "application/json" },
+                "body": JSON.stringify(voluntarioNuevo)
+            });
+            if (res.status == 201) {
+                console.log("Creado!");
+            }
+            mostrarTabla();
+        }
+        catch (error) {
+            console.log(error);
+        }  
+    }
+    function agrega3(e){
+        e.preventDefault();
+        for(let i=0; i<3; i++){
+            agrega1(e);
+        }
+        mostrarTabla();
+    }
+    async function mostrarTabla(){
+        try{
+            let respuesta = await fetch(url);
+            let voluntarios= await respuesta.json();
+            let tabla=document.querySelector("#t-voluntarios");
+            tabla.innerHTML=`
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Telefono</th>
+                        <th>email</th>
+                        <th>Area</th>
+                        <th>Turno</th>
+                    </tr>
+                </thead>
+                `;
+            let cuerpo=document.createElement("tbody");
+            for (let i = 0; i < voluntarios.length; i++){
+                let fila=document.createElement("tr");
+                fila.innerHTML = `
+                        <td>${voluntarios[i].nombre}</td>
+                        <td>${voluntarios[i].apellido}</td> 
+                        <td>${voluntarios[i].telefono}</td> 
+                        <td>${voluntarios[i].email}</td> 
+                        <td>${voluntarios[i].area}</td> 
+                        <td>${voluntarios[i].turno}</td>
+                        <td><button class="fas fa-edit" value=${i}></button></td>
+                        <td><i class="fas fa-trash-alt"></i></td>
+                    `;
+                cuerpo.appendChild(fila);
+            }
+            tabla.appendChild(cuerpo);
+            let btn_editar=document.querySelectorAll(".fa-edit");
+            for (const btn of btn_editar) {
+                btn.addEventListener("click",function(e){editar(e,voluntarios)})
+            }
+        }catch(error){
+        console.log(error);
+    }
+    }
+    function borrar(e){
+        e.preventDefault();
+        voluntarios=[];   
+        mostrarTabla();
+    }
+    async function editar(e,voluntarios){
+        e.preventDefault();
+        let fila=e.target.parentElement.parentElement;
+        let i=e.target.value;
+        fila.innerHTML=`
+            <form id="form-edit">
+                <td><input type="text" id="edit-nombre" value="${voluntarios[i].nombre}"></td>
+                <td><input type="text" id="edit-apellido" value="${voluntarios[i].apellido}"></td>
+                <td><input type="text" id="edit-telefono" value="${voluntarios[i].telefono}"></td>
+                <td><input type="text" id="edit-email" value="${voluntarios[i].email}"></td>
+                <td><input type="text" id="edit-area" value="${voluntarios[i].area}"></td>
+                <td><input type="text" id="edit-turno" value="${voluntarios[i].turno}"></td>
+                <td><button class="fas fa-check-circle" name="id" value="${voluntarios[i].id}"></button></td>
+            </form>
+                `;
+        document.querySelector(".fa-check-circle").addEventListener("click",async function(e){
+            voluntarios[i].nombre=document.querySelector("#edit-nombre").value;
+            voluntarios[i].apellido=document.querySelector("#edit-apellido").value;
+            voluntarios[i].telefono=document.querySelector("#edit-telefono").value;
+            voluntarios[i].email=document.querySelector("#edit-email").value;
+            voluntarios[i].area=document.querySelector("#edit-area").value;
+            voluntarios[i].turno=document.querySelector("#edit-turno").value;
+            try {
+                let res = await fetch(`${url}/${voluntarios[i].id}`, {
+                    "method": "PUT",
+                    "headers": { "Content-type": "application/json" },
+                    "body": JSON.stringify(voluntarios[i])
+                });
+                if (res.status == 201) {
+                    console.log("Creado!");
+                }
+                mostrarTabla();
+            }
+            catch (error) {
+                console.log(error);
+            }  
+        })
+    }
 }
