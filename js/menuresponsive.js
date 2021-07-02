dbfd9e9f6ed917dea0955171c9192109af21f9c1
EmaBrites <71233604+EmaBrites@users.noreply.github.com>
2021-07-01 22:29:39 -0300

    Ultima versión con partial render

--- js/menuresponsive.js
+++ js/menuresponsive.js
@@ -8,21 +8,4 @@ function iniciarMenu(){
         boton.classList.toggle("change");
         document.querySelector("nav ul").classList.toggle("show");
     }
-    // Leer mas
-    let leer=document.querySelectorAll("#btn-leer");
-    for (const item of leer) {
-        item.addEventListener("click",toggleTexto);
-    }
-    function toggleTexto(e){
-        let botonLeer=e.target;
-        let padre=botonLeer.parentElement;
-        let ocultos=padre.querySelectorAll(".mostrar");
-        for (const texto of ocultos) {
-            texto.classList.toggle("oculto");
-        }
-        if (ocultos[0].classList=="mostrar"){
-            botonLeer.innerHTML="Leer menos";
-        }
-        else{ botonLeer.innerHTML="Leer mas"}
-    }
 }
